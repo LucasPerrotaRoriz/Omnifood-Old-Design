2aaa9217c3e2badb85ba4020f5fdd35c8a073c40
LucasPerrotaRoriz <LucasPR1998@protonmail.com>
2022-11-07 16:43:02 -0300

    Smooth scrolling

--- Omnifood-Old-Design/resources/js/script.js
+++ Omnifood-Old-Design/resources/js/script.js
@@ -5,3 +5,14 @@ btnHamburger.addEventListener("click", function () {
   btnHamburger.classList.toggle("is-active");
   mobileNav.classList.toggle("is-active--mobile");
 });
+
+document.querySelector('.hero__navigation').addEventListener('click', function (e) {
+  e.preventDefault();
+  console.log('hey')
+
+  // Matching strategy
+  if (e.target.classList.contains('hero__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
